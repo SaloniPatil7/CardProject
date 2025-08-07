18671eebfb28b2a0e451b8bd1bf4169b5918e782
Saloni Patil <saloniapatil1234@gmail.com>
2025-08-07 14:33:10 +0530

    change DatabaseLink to DeployesDB link

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,32 @@ const app = express()
 const mongoose = require('mongoose')
 const Card = require('./models/CardModel.js')
 const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+let MongoDB=process.env.MongoDB;
+const PORT=process.env.PORT || 3000
+console.log(MongoDB);
+
 async function ConnectDB() {
-    mongoose.connect('mongodb://127.0.0.1:27017/Cards')
+    mongoose.connect(MongoDB)
         .then(() => console.log('connected'))
         .catch((err) => console.log(err));
 }
 ConnectDB();
 
-app.listen('3000', () => {
+app.listen(PORT, () => {
     console.log("listening");
 })
 
-app.post('/add', (req, res) => {
+app.post('/add', async (req, res) => {
     try {
         let data = req.body;
         console.log(data);
         let newCard = new Card(data);
-        newCard.save();
+        await newCard.save();
         res.send(data);
     } catch (err) {
         console.log(err)
@@ -33,7 +39,7 @@ app.get('/data', async (req, res) => {
     try {
         let data = await Card.find({});
         if(!data){
-            res.send('no data');
+             return res.send('no data');
         }
         console.log(data)
         res.send(data);
